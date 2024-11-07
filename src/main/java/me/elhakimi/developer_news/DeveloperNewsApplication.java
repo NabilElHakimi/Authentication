7c078bb29e8cbec414c2e56389bfb0f83b788e45
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-11-07 17:18:27 +0100

    first commit

---
+++ src/main/java/me/elhakimi/developer_news/DeveloperNewsApplication.java
@@ -0,0 +1,15 @@
+package me.elhakimi.developer_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperNewsApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/me/elhakimi/developer_news/DeveloperNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elhakimi.developer_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
